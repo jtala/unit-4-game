66e058fdbcb705aedcb08a120a87170ebea0c9d0
Jonathan Talavera <jtalavera@g.ucla.edu>
2018-11-28 22:26:23 -0800

    first workings

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+// There is a randomly generated number from 19-120.
+
+var tumToTarget = 30;
+
+$("#targNum").text(targNum);
+
+// There are four crystals. On click each crystal generates a random number from 1-12.
+
+    // The first click is stored in myScore.
+
+        // Every crystal click after that adds to myScore.
+
+        // if myScore > randScore. Reset the loop.
+
+
+
+
+
+
+
+
+
+
+
+
+
