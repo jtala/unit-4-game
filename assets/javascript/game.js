23457194cab1f351cc8f58af9da7e53a7ee6fd3d
Jonathan Talavera <jtalavera@g.ucla.edu>
2018-11-29 22:24:54 -0800

    Final Edition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,90 @@
 
-// There is a randomly generated number from 19-120.
+    // Number user has to get. Randomly ranges from 19-120.
+    var numToTarget = [Math.floor(Math.random() * 101)+19];
+    $("#targNum").text(numToTarget);
+    
+     // Number that you start with.
+    var myScore = 0;
 
-var tumToTarget = 30;
+    var wins = 0;
+    var losses = 0;
+    
+    // 4 crystal images.
+    var imgArray = [
+    "https://www.colourbox.com/preview/4545062-red-crystal.jpg",
+    "https://images-na.ssl-images-amazon.com/images/I/51snWRfynML._SL500_.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLWdoqVf8dP5AjAWtN4zUmEmeUI38D7uB-k1JF59MpptBVAXDlmw",
+    "https://vignette.wikia.nocookie.net/thelorienlegacies/images/7/71/Green_Wind_Crystal.PNG/revision/latest?cb=20160610041134"
+    ];
+    //Wrapping a reset button. Everytime I tried to do this, it broke the code!
 
-$("#targNum").text(targNum);
+    // function reset(){} that encapsulated the whole thing from here on down.
+    
 
-// There are four crystals. On click each crystal generates a random number from 1-12.
+    // Array that has random values from 0-12.
+    var randomArr = Array.from({length: 4}, () => Math.floor(Math.random() * 12)+1);
+    console.log(randomArr);
 
-    // The first click is stored in myScore.
+    // For loop  to generate crystal images.
+    for (var i = 0; i < 4; i++) {
+        // giving each Crystal an img tag.
+        var imageCrystal = $("<img>");
+        imageCrystal.attr("src", imgArray[i]);
+        imageCrystal.addClass("crystal-image");
+        
+        // giving each crystal a random number value.
+        imageCrystal.attr("data-crystalvalue", randomArr[i]);
+        $(".crystals").append(imageCrystal);
 
-        // Every crystal click after that adds to myScore.
+    }
 
-        // if myScore > randScore. Reset the loop.
+    // On click function.
+    $(".crystal-image").on("click", function() {
+    
+        // Look into the individual crystal that was clicked.
+        var crystalValue = ($(this).attr("data-crystalvalue"));
 
+        // Extract the number value from it.
+        crystalValue = parseInt(crystalValue);
+
+        //Add it to my Score.
+        myScore += crystalValue;
+        
+        
+        
+        // Win/ Lose Condition.
+        if (myScore === parseInt(numToTarget)){
+            alert("Woohoo, you won!");
+            wins++;
+            //resets score to 0
+            $("#myNum").text(myScore = 0);
+            //Gives crystals new values. I couldn't figure out how to do this!
+            
+           
+        }
+        else if (myScore >= numToTarget){
+            alert("Oops! You went over.");
+            losses++;
+            $("#myNum").text(myScore = 0);
+            //Gives crystals new values. I couldn't figure out how to do this!
+    
+            
+        }
+    
+    // update html.
+    $("#myNum").text(myScore);
+    $("#myWins").text(wins);
+    $("#myLosses").text(losses);
+
+    
+
+    });
+
+
+    
+
+    
+   
 
 
 
